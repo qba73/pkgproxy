88959b7f22e1bf2a88529530cf1e74b85441b79d
Jakub Jarosz <jakub@jarosz.dev>
2025-07-02 10:41:23 +0100

    update docs
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

--- doc.go
+++ doc.go
@@ -1,2 +1,4 @@
-// Package pkgproxy provides functions for getting Go package data from pkg.go.dev.
+/*
+Package pkgproxy provides functions for retrieving information about Go packages from pkg.go.dev.
+*/
 package pkgproxy
